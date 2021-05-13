312901bd6cf463fca553449a183d99139aa48155
maayami <mayank16meena@gmail.com>
2021-05-13 20:15:57 +0530

    update url

--- Simplab_API/models.py
+++ Simplab_API/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=25)
+    username = models.CharField(max_length=25, unique=True)
     password = models.CharField(max_length=25)
     email = models.EmailField(blank=True)
 
